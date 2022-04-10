23fba1d3f821eea56c0ddf0e5a1145cad13f2591
Chris Rodea <ChrisRodea@mail.fresnostate.edu>
2022-04-10 14:54:54 -0700

    Created weather.py , a logins.py , and .gitignore file. Only file being pushed to repo is weather.py

---
+++ weather.py
@@ -0,0 +1,6 @@
+import logins
+
+#* API key to access OpenWeatherMap is imported from logins.py file
+#* The key is not included directly in main file because it is sensitive data, and should not be committed to public repository.
+API_KEY = logins.api_key 
+
